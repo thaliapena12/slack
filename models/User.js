c1d2bb851d92dc5fbbe3c52e5ff2fd9e678073a5
Javier Ortiz <javier.ortiz989@gmail.com>
2020-02-10 15:23:37 -0800

    Delete sessionToken from User model

--- models/User.js
+++ models/User.js
@@ -13,10 +13,6 @@ const UserSchema = new Schema({
     password: {
         type: String,
         required: true
-    },
-    sessionToken: {
-        type: String,
-        // required: true
     }
 }, { timestamps: true })
 
