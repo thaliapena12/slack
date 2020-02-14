773a0b826472f11a4ea299a5e743cc654cbe5ee5
Javier Ortiz <javier.ortiz989@gmail.com>
2020-02-14 10:22:24 -0800

    Sets default current channel after fetching for user channels

--- frontend/src/reducers/current_channel_reducer.js
+++ frontend/src/reducers/current_channel_reducer.js
@@ -1,9 +1,12 @@
 import { RECEIVE_CURRENT_CHANNEL } from '../actions/channel_actions';
+import { RECEIVE_USER_CHANNELS } from "../actions/user_actions";
 
 export default function currentChannelReducer(state = null, action) {
     switch (action.type) {
         case RECEIVE_CURRENT_CHANNEL:
             return action.channel;
+        case RECEIVE_USER_CHANNELS:
+            return action.channels[0];
         default:
             return state;
     }
