271fe7562af536155fe120cdf33b04391f42176f
Javier Ortiz <javier.ortiz989@gmail.com>
2020-02-20 14:13:47 -0800

    Handle errors for adding people to channel form

--- frontend/src/components/channels/people_to_channel.js
+++ frontend/src/components/channels/people_to_channel.js
@@ -7,8 +7,9 @@ import userPlaceHolder from '../dmgroups/user_placeholder.png';
 class PeopleToChannel extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { usersList: {}, selectedUsers: [] };
+        this.state = { usersList: {}, selectedUsers: [], errors: "" };
         this.addPeople = this.addPeople.bind(this);
+        this.closeError = this.closeError.bind(this);
     }
 
     componentDidMount() {
@@ -21,7 +22,12 @@ class PeopleToChannel extends React.Component {
         return (e) => {
             e.preventDefault(e);
             const newState = this.state.selectedUsers;
-            if (!newState.includes(user)) newState.push(user);
+            if (this.props.currentChannel.channelMembers.includes(user._id)) {
+                this.setState({ errors: "This user is already a member of the channel."})
+            } else if (!newState.includes(user)) {
+                newState.push(user);
+                this.setState({ errors: "" });
+            }
             this.setState({ selectedUsers: newState });
         }
         
@@ -46,8 +52,12 @@ class PeopleToChannel extends React.Component {
         this.props.closeModalForm();
     }
 
+    closeError (e) {
+        e.preventDefault();
+        this.setState({ errors: "" });
+    }
+
     render() {
-        
         const { usersList, selectedUsers } = this.state;
 
         if (usersList.length > 0) {
@@ -55,6 +65,12 @@ class PeopleToChannel extends React.Component {
             return (
                 <div className="channel-form-container">
                     <div className="newdm">
+                        { 
+                            Object.values(this.state.errors).length !== 0 &&
+                            <ul className="people-to-channel-errors">
+                                <li><span>&#9888;</span>{this.state.errors}<button onClick={this.closeError}>x</button></li>    
+                            </ul>
+                        }
                         <div className="newdm-header">
                             <div onClick={this.props.closeModalForm} className="close">&times;</div>
                             <h3> Add people to channel</h3>
@@ -77,7 +93,11 @@ class PeopleToChannel extends React.Component {
                                     </ul>
                                 </div>
                             </div>
-                            <button onClick={this.addPeople}>Go</button>
+                            <button 
+                                onClick={this.addPeople} 
+                                disabled={this.state.selectedUsers.length === 0}
+                                id={this.state.selectedUsers.length === 0 ? "disabled" : ""}
+                                >Go</button>
                         </div>
                         <div className="newdm-users-list">
                             <ul>
