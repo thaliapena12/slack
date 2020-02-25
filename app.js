e43e4fba91e58e6ef1163bbc439fe3f203aafbde
Yenisbel Valle <yenisbelv@gmail.com>
2020-02-24 21:13:55 -0800

    change default port

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ mongoose
 
 app.get("/", (req, res) => res.send("Hello"));
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
