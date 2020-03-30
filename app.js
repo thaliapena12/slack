a9f53a19b742e71ec0f54ea6b2f075558ec2caa5
Javier Ortiz <javier.ortiz989@gmail.com>
2020-03-30 14:23:25 -0700

    Fix package-lock.json

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ mongoose
 
 app.get("/", (req, res) => res.send("Hello"));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const server = app.listen(port, () => console.log(`Server is running on port ${port}`));
 
