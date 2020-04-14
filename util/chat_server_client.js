66b7bb68c8816651c0d446de03490c6640ffbf12
Javier Ortiz <javier.ortiz989@gmail.com>
2020-04-14 13:58:51 -0700

    Create chat server client

---
+++ util/chat_server_client.js
@@ -0,0 +1,29 @@
+const socketIOClient = require("socket.io-client");
+const Channel = require('../models/Channel');
+const CHAT_SERVER_ENDPOINT = require("../config/keys").CHAT_SERVER_ENDPOINT;
+
+// to be set dynamically in production with process.env.CHAT_SERVER_URL
+const endpoint = CHAT_SERVER_ENDPOINT;
+
+class ChatServerClient {
+    constructor() {
+        this.socket = socketIOClient(endpoint);
+    }
+
+    dispatchReceiveMessage(messageChannel, message, authorId) {
+        // fetch the conversation in order to get its participants
+        Channel.findOne(messageChannel).exec((err, channel) => {
+            if (err) throw err;
+            // tell the chat server what message was sent between which people
+            let payload = {
+                participants: channel.channelMembers,
+                message,
+                authorId
+            }
+            console.log("IN")
+            this.socket.emit('broadcast_message', payload);
+        })
+    }
+}
+
+module.exports = ChatServerClient;
